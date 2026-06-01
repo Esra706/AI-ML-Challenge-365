4234176b1070e45548540d7d48c9b8e47439b9a6
Esra706 <isramemon0786@gmail.com>
2026-06-01 22:33:20 +0500

    Arthmatic Opreators

---
+++ Day4_Numbers & Math/Arithmetic Operators.py
@@ -0,0 +1,12 @@
+num1=10
+num2=3
+
+print(num1+num2)
+print(num1-num2)
+print(num1*num2)
+print(num1/num2)
+print(num1%num2)
+print(num1//num2)  #floor division : remove decimal values
+print(num1**num2)
+
+
