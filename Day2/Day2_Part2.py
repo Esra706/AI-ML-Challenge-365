da8b2047380aeec7232cd595bec096d67c0503fc
Esra706 <isramemon0786@gmail.com>
2026-06-01 11:43:18 +0500

    Day 1 challenge

---
+++ Day2/Day2_Part2.py
@@ -0,0 +1,24 @@
+# Type Casting
+# String to int 
+in_string="5"
+real=int(in_string)
+print(real)
+print(in_string)
+print(real+5)
+
+# Float to int
+pi=3.14
+integer_value=int(pi)
+print(integer_value)
+print(pi)
+
+#int to string
+score=95
+score_in_txt=str(score)
+print(score_in_txt)
+print("my score is :"+score_in_txt) 
+
+#int to float
+num=100
+num_in_float=float(num)
+print(num_in_float)
---
+++ Day2/Day2_part1.py
@@ -0,0 +1,33 @@
+#Practice 5 examples of each data type
+
+# int
+age=20
+Salary=00
+Marks=90
+Day=2
+Money=10
+print(age,Salary,Marks,Day,Money)
+
+#Float
+Hight=41.2
+pi=3.14
+Gravity=9.8
+Percentage=74.5
+Hight_in_Feet=5.8
+print(Hight,Hight_in_Feet,Percentage,Gravity,pi)
+
+#Boolean
+Is_logged_in= True
+has_passed=False
+Light_on=True
+is_weekend=False
+user=True
+print(Is_logged_in,has_passed,Light_on,is_weekend,user)
+
+#Strings
+Name="Isra Memon"
+Semster="4th"
+Age='20'
+Status="Student"
+Degree="BSSE"
+print(Name,Semster,Age,Status,Degree)
