11dc1c2a86c3f997709d8c1ac1443ecddbd654e7
Esra706 <isramemon0786@gmail.com>
2026-06-01 13:15:07 +0500

    Updated code complete

--- Day3/Day3_P2.py
+++ Day3/Day3_P2.py
@@ -2,4 +2,8 @@
 name="PythonLearning"
 print(name[0:5]) # Print PYTHO
 
-print(name[:]) 
+print(name[:]) # by self considering from 0 to last index 
+
+
+reversed_name = name[::-1]
+print(reversed_name)  # output "gninraeLnohtyP"
