875f810b0839cf5ef9d52bd20459613e113f5565
Esra706 <isramemon0786@gmail.com>
2026-06-01 23:48:34 +0500

    Updated Code

---
+++ Day4_Numbers & Math/Math Module.py
@@ -0,0 +1,14 @@
+import math
+x=5
+y=12.423
+print(math.sqrt(x))
+print(math.pi)
+print(math.floor(y))
+
+#In built function 
+a=90.00
+b=-20
+print(round(a))
+print(abs(b))
+print(max(10,20,30,40))
+print(min(10,20,30,40))
