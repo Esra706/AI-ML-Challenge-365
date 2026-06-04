2f9dca339d9f7aac5d3a23dc4db36ef2a0c33a90
Esra706 <isramemon0786@gmail.com>
2026-06-04 16:05:09 +0500

    Nested List

---
+++ Day5_Lists/Nested_List.py
@@ -0,0 +1,17 @@
+mobiles = [
+    ["iPhone 13", "iPhone 14"],  # Index 0
+    ["S22 Ultra", "S23 Ultra"]   # Index 1
+]
+print(mobiles[1][1])
+
+Numbers=[1,2,3,4,5,6,7,8,9,10]
+print(Numbers.remove(2))
+print(Numbers)
+print(Numbers.remove(4))
+print(Numbers) 
+print(Numbers.remove(6))
+print(Numbers) 
+print(Numbers.remove(8))
+print(Numbers) 
+print(Numbers.remove(10))
+print(Numbers)           
